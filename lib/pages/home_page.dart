fd2bc30ca080a1cf200c6671632dad6c752053c0
Nicholaus Adhyatma <nicholausadhyatma@gmail.com>
2023-01-30 21:48:39 +0700

    model

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,83 @@
+import 'dart:math';
+import 'package:faker/faker.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_2/models/product_model.dart';
+
+// ignore: must_be_immutable
+class HomePage extends StatelessWidget {
+  HomePage({super.key});
+  List<Product> myProduct = List.generate(
+    10,
+    (index) => Product(
+      faker.food.restaurant(),
+      faker.lorem.sentence(),
+      "https://picsum.photos/id/${index + Random(0).nextInt(100)}/200",
+      10000 + Random().nextInt(100000),
+    ),
+  );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Model"),
+      ),
+      body: GridView.builder(
+          padding: const EdgeInsets.all(10.0),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10,
+          ),
+          itemCount: myProduct.length,
+          itemBuilder: ((context, index) {
+            return Container(
+        
+              // ignore: prefer_const_constructors
+              decoration:  BoxDecoration(
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(10.0),
+                ),
+              ),
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    width: 100,
+                    child: Image.network(myProduct[index].imageUrl),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    myProduct[index].title,
+                    maxLines: 1,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Rp. ${myProduct[index].price}",
+                    style: const TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    myProduct[index].description,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  )
+                ],
+              ),
+            );
+          })),
+    );
+  }
+}
